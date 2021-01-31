69f7fbdd1f6d8d7d86682780da9d2d03c9f2e0f4
Egi Muhamad Saefulhaqi <egi.msaefulhaqi@gmail.com>
2021-01-31 21:37:58 +0700

    add dockerfile

--- src/main/java/com/doku/live/coding/dokutest/repositories/BankRepository.java
+++ src/main/java/com/doku/live/coding/dokutest/repositories/BankRepository.java
@@ -3,5 +3,5 @@ package com.doku.live.coding.dokutest.repositories;
 import com.doku.live.coding.dokutest.entity.Bank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BankRepository extends JpaRepository<Bank, Long> {
+public interface BankRepository extends JpaRepository<Bank, Integer> {
 }
